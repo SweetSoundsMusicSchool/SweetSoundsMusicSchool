e88e58c9cb5dba14124ac530a2b2e580224c4fb3
SweetSoundsMusicSchool <122542216+SweetSoundsMusicSchool@users.noreply.github.com>
2023-02-01 13:23:00 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Capstone1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Capstone1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GotoReg()
+        {
+            return View("Registration");
+        }
+
+        [HttpPost]
+
+        public IActionResult GotoReg(RegisterForm rf)
+        {
+            return View("Registration");
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RegisterForm.cs
@@ -0,0 +1,19 @@
+﻿namespace Capstone1.Models
+{
+    public class RegisterForm
+    {
+        public string ParentName { get; set; } = string.Empty;
+
+        public string ChildName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string ChildAge { get; set; } = string.Empty;
+
+        public string ContactPreference { get; set; } = string.Empty;
+
+        public bool? NewCustomer { get; set; } 
+    }
+}
