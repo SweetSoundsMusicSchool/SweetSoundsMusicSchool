8bcd815abdcae9fc37541a112952012294017da9
Aleks <becharal@sheridancollege.ca>
2023-09-30 11:06:51 -0400

    test #3, connection string with password/user

--- obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++ obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//function to auto add dashes to phone input feild on registration page
+function addDashes(f) {
+    f_val = f.value.replace(/\D[^\.]/g, "");
+    f.value = f_val.slice(0, 3) + "-" + f_val.slice(3, 6) + "-" + f_val.slice(6);
+}
+
+
+//for checkout
+function checkout(pubKey, sessionId) {
+    const stripe = Stripe(pubKey);
+    stripe.redirectToCheckout({ sessionId });
+}
