3342d78e3cc3e29a62421f01948017d88f53a71f
osamashalabi1 <osama.shalabi2001@gmail.com>
2023-10-11 10:25:05 -0400

    Admin Log in

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Capstone1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin model)
+        {
+            if (model.Username == "admin" && model.Password == "admin123")
+            {
+                
+                return View("ClassList");
+            }
+
+            
+           // ModelState.AddModelError("", "Invalid username or password");
+            return View("AdminLogIn" , model);
+        }
+    }
+}
