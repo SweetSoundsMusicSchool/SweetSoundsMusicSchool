a628edbaf0d961770c67d313155cd9d86875ca59
Aleks <becharal@sheridancollege.ca>
2023-10-31 15:05:49 -0400

    class picker form changed to radio buttons

--- Models/ClassChoosenDetails.cs
+++ Models/ClassChoosenDetails.cs
@@ -6,22 +6,55 @@ namespace Capstone1.Models
     {
         public string Location { get; set; } = string.Empty;
 
+        public string Lesson { get; set; } = String.Empty;
+
+        //public string ClassDate { get; set; } = String.Empty;
+
+
+
+        public List<SelectListItem> All { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {
+                Value = "Geogrgetown, Children 0 to 4 Years",
+                Text = "Geogrgetown,  Children 0 to 4 Years. \n Tuesdays @ 10:30 AM"
+            },
+            new SelectListItem {
+                Value = "Geogrgetown, Children 0 to 18 Months",
+                Text = "Geogrgetown, Children 0 to 18 Months, Tuesdays @ 11:45 AM"
+            },
+            new SelectListItem {
+                Value = "Oakville, Children 0 to 4 Years",
+                Text = "Oakville, Children 0 to 4 Years. Wednesdays @ 10:30 AM"
+            },
+            new SelectListItem {
+                Value = "Oakville, Children 0 to 18 Months",
+                Text = "Oakville, Children 0 to 18 Months. Wednesdays @ 11:45 AM"
+            },
+             new SelectListItem {
+                Value = "Milton, Children 0 to 4 Years",
+                Text = "Milton, Children 0 to 4 Years. Thursdays @ 10:45 AM"
+            },
+              new SelectListItem {
+                Value = "Milton, Children 0 to 18 Months",
+                Text = "Milton, Children 0 to 18 Months. Wednesdays @ 11:45 AM"
+            },
+        };
         public List<SelectListItem> Locations { get; } = new List<SelectListItem>
         {
             new SelectListItem {
                 Value = "Milton",
-                Text = "Milton"
+                Text = "Milton, Thursdays"
             },
             new SelectListItem {
                 Value = "Geogrgetown",
-                Text = "Geogrgetown"
+                Text = "Geogrgetown, Tuesdays"
             },
             new SelectListItem {
                 Value = "Oakville",
-                Text = "Oakville"  },
+                Text = "Oakville, Wednesdays"  },
         };
 
-        public string Lesson { get; set; } = String.Empty;
+       
         public List<SelectListItem> Lessons { get; } = new List<SelectListItem>
         {
             new SelectListItem { 
